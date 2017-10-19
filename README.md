09706ad1ecab76f8cac92734a7e77b0da104a420
Reed <reedpo@hotmail.ca>
2017-10-19 17:02:32 -0600

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Calculator
+----------
+
+The point of this simple calculator is to become more familiar with python
+and to become reaquainted with it. Has addition, subtraction, multiplication,
+division, modulo, power-of and square root functionality. 
---
+++ calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""A calculator program, as well as my first attempt
+at working with python in years."""
+
+__author__	= "Reed Posehn"
+__copyright__	= "Written in 2017"
+
+import sys
+import math
+
+def main():
+   print("Test")
+
+if __name__ == '__main__':
+    main() 
