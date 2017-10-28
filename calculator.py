fd51dbc1bee01776b2d73328da2fab77dd4b70fd
Reed <reedpo@hotmail.ca>
2017-10-28 14:58:53 -0600

    Wrote add function

--- calculator.py
+++ calculator.py
@@ -9,8 +9,17 @@ __copyright__	= "Written in 2017"
 import sys
 import math
 
+def add(num1, num2):
+    res = num1 + num2
+    return res
+
+def sub(num1, num2):
+    res = num1 - num2
+    return res
+
 def main():
-   print("Test")
+    op =  raw_input("Which operation would you like to do? ")
+    
 
 if __name__ == '__main__':
-    main() 
+    main()
